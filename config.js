14ed71ad6692fe4acfd105c46fa63db5dc356790
jan0artemis-collab <jan0artemis@gmail.com>
2026-01-30 14:04:58 +0700

    Update API URL in config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 const CONFIG = {
     // URL Google Apps Script Anda yang sudah di-deploy
-    API_URL: 'https://script.google.com/macros/s/AKfycbzuUUh6AAT4KefYkCYzEZ5VU-ne8dzK5qHgZoXOHPn3L0idGE1Oh8Ds5FEB6WYqs-wI/exec',
+    API_URL: 'https://script.google.com/macros/s/AKfycbwTJgvmRPR8ycMjrg5nbRIyka6C4z213AoCIACYUPuDG-wj4toUDMtOIoKuFPjoFRSt/exec',
     
     // UBAH MENJADI true UNTUK MENGGUNAKAN DATABASE SPREADSHEET!
     USE_API: true
@@ -14,3 +14,4 @@ const CONFIG = {
 function isValidApiUrl() {
     return CONFIG.USE_API === true && CONFIG.API_URL && CONFIG.API_URL !== 'PLACEHOLDER';
 }
+
